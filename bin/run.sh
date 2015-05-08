8a197629e8a53af15323f3195a8fd872e934dee0
sukrit007 <sukrit007@gmail.com>
2015-05-08 00:11:44 -0700

    Fix uwsgi issue

--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,18 @@
-#!/bin/bash -c
+#!/bin/bash -ex
 
-export ETCD_HOST='${ETCD_HOST:-172.17.42.1}'
-export ETCD_PORT='${ETCD_PORT:-4001}'
-export ETCD_TOTEM_BASE='${ETCD_TOTEM_BASE:-/totem}'
-export API_EXECUTORS='${API_EXECUTORS:-2}'
-export GITHUB_TOKEN='${GITHUB_TOKEN}'
-export CLUSTER_NAME='${CLUSTER_NAME:-local}'
-export TOTEM_ENV='${TOTEM_ENV:-local}'
-export ENCRYPTION_PASSPHRASE='${ENCRYPTION_PASSPHRASE:-changeit}'
-export ENCRYPTION_S3_BUCKET='${ENCRYPTION_S3_BUCKET:-not-set}'
-export ENCRYPTION_STORE='${ENCRYPTION_PROVIDER:-s3}'
-export LOG_IDENTIFIER='${LOG_IDENTIFIER:-configservice}'
+export ETCD_HOST="${ETCD_HOST:-172.17.42.1}"
+export ETCD_PORT="${ETCD_PORT:-4001}"
+export ETCD_TOTEM_BASE="${ETCD_TOTEM_BASE:-/totem}"
+export API_EXECUTORS="${API_EXECUTORS:-2}"
+export GITHUB_TOKEN="${GITHUB_TOKEN}"
+export CLUSTER_NAME="${CLUSTER_NAME:-local}"
+export TOTEM_ENV="${TOTEM_ENV:-local}"
+export ENCRYPTION_PASSPHRASE="${ENCRYPTION_PASSPHRASE:-changeit}"
+export ENCRYPTION_S3_BUCKET="${ENCRYPTION_S3_BUCKET:-not-set}"
+export ENCRYPTION_STORE="${ENCRYPTION_PROVIDER:-s3}"
+export LOG_IDENTIFIER="${LOG_IDENTIFIER:-configservice}"
+
+python --version
 
 /usr/local/bin/uwsgi \
         --master \
