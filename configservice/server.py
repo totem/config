89d7ba7fcf704d7d123d127d831fd72e7d258eed
sukrit007 <sukrit007@gmail.com>
2015-05-08 10:29:03 -0700

    add error view

--- configservice/server.py
+++ configservice/server.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from flask import Flask
 from flask.ext.cors import CORS
 from conf.appconfig import CORS_SETTINGS
-from configservice.views import root, hypermedia, provider, config
+from configservice.views import root, hypermedia, provider, config, error
 
 app = Flask(__name__)
 
@@ -12,5 +12,5 @@ hypermedia.register_schema_api(app).register_error_handlers(app)
 if CORS_SETTINGS['enabled']:
     CORS(app, resources={'/*': {'origins': CORS_SETTINGS['origins']}})
 
-for module in [root, provider, config]:
+for module in [root, provider, config, error]:
     module.register(app)
